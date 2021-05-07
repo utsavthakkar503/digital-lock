892200f1df22ddf16fa451db04ffdc5e3b126362
paranshu1234 <paranshu1234@gmail.com>
2021-05-08 02:17:29 +0530

    added SVG lock animation on lock or unlock

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,70 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import { SpinButton, ISpinButtonStyles } from "@fluentui/react/lib/SpinButton";
-import { ThemeProvider, createTheme, PartialTheme } from '@fluentui/react/lib/Theme';
-import { Toggle } from '@fluentui/react/lib/Toggle';
+import {
+  ThemeProvider,
+  createTheme,
+  PartialTheme,
+} from "@fluentui/react/lib/Theme";
+import { Toggle } from "@fluentui/react/lib/Toggle";
 import { Stack, IStackTokens } from "@fluentui/react";
-import { DefaultButton, PrimaryButton } from '@fluentui/react/lib/Button';
+import { DefaultButton, PrimaryButton } from "@fluentui/react/lib/Button";
 import "./App.css";
-import Lock from "./Lock"
+import Lock from "./Lock";
 
 const lightTheme: PartialTheme = createTheme({
   palette: {
-    themePrimary: '#0756f2',
-    themeLighterAlt: '#f5f8fe',
-    themeLighter: '#d6e3fd',
-    themeLight: '#b2cafb',
-    themeTertiary: '#6797f7',
-    themeSecondary: '#2469f4',
-    themeDarkAlt: '#074dda',
-    themeDark: '#0641b8',
-    themeDarker: '#043088',
-    neutralLighterAlt: '#f8f8f8',
-    neutralLighter: '#f4f4f4',
-    neutralLight: '#eaeaea',
-    neutralQuaternaryAlt: '#dadada',
-    neutralQuaternary: '#d0d0d0',
-    neutralTertiaryAlt: '#c8c8c8',
-    neutralTertiary: '#595959',
-    neutralSecondary: '#373737',
-    neutralPrimaryAlt: '#2f2f2f',
-    neutralPrimary: '#000000',
-    neutralDark: '#151515',
-    black: '#0b0b0b',
-    white: '#ffffff',
-  }});
+    themePrimary: "#0756f2",
+    themeLighterAlt: "#f5f8fe",
+    themeLighter: "#d6e3fd",
+    themeLight: "#b2cafb",
+    themeTertiary: "#6797f7",
+    themeSecondary: "#2469f4",
+    themeDarkAlt: "#074dda",
+    themeDark: "#0641b8",
+    themeDarker: "#043088",
+    neutralLighterAlt: "#f8f8f8",
+    neutralLighter: "#f4f4f4",
+    neutralLight: "#eaeaea",
+    neutralQuaternaryAlt: "#dadada",
+    neutralQuaternary: "#d0d0d0",
+    neutralTertiaryAlt: "#c8c8c8",
+    neutralTertiary: "#595959",
+    neutralSecondary: "#373737",
+    neutralPrimaryAlt: "#2f2f2f",
+    neutralPrimary: "#000000",
+    neutralDark: "#151515",
+    black: "#0b0b0b",
+    white: "#ffffff",
+  },
+});
 
+const darkTheme: PartialTheme = createTheme({
+  palette: {
+    themePrimary: "#16cfd9",
+    themeLighterAlt: "#010809",
+    themeLighter: "#032123",
+    themeLight: "#073e41",
+    themeTertiary: "#0d7c82",
+    themeSecondary: "#13b6bf",
+    themeDarkAlt: "#2ad4dd",
+    themeDark: "#47dae2",
+    themeDarker: "#74e4ea",
+    neutralLighterAlt: "#201f1e",
+    neutralLighter: "#201f1e",
+    neutralLight: "#1e1e1d",
+    neutralQuaternaryAlt: "#1c1b1b",
+    neutralQuaternary: "#1b1a19",
+    neutralTertiaryAlt: "#1a1918",
+    neutralTertiary: "#f5f9fd",
+    neutralSecondary: "#f6fafe",
+    neutralPrimaryAlt: "#f8fbfe",
+    neutralPrimary: "#eff6fc",
+    neutralDark: "#fbfdfe",
+    black: "#fdfeff",
+    white: "#201f1e",
+  },
+});
 
-  const darkTheme: PartialTheme = createTheme({
-    palette: {
-      themePrimary: '#16cfd9',
-      themeLighterAlt: '#010809',
-      themeLighter: '#032123',
-      themeLight: '#073e41',
-      themeTertiary: '#0d7c82',
-      themeSecondary: '#13b6bf',
-      themeDarkAlt: '#2ad4dd',
-      themeDark: '#47dae2',
-      themeDarker: '#74e4ea',
-      neutralLighterAlt: '#201f1e',
-      neutralLighter: '#201f1e',
-      neutralLight: '#1e1e1d',
-      neutralQuaternaryAlt: '#1c1b1b',
-      neutralQuaternary: '#1b1a19',
-      neutralTertiaryAlt: '#1a1918',
-      neutralTertiary: '#f5f9fd',
-      neutralSecondary: '#f6fafe',
-      neutralPrimaryAlt: '#f8fbfe',
-      neutralPrimary: '#eff6fc',
-      neutralDark: '#fbfdfe',
-      black: '#fdfeff',
-      white: '#201f1e',
-    }});
-  
-  
-
-  
 const containerStackTokens: IStackTokens = { childrenGap: 0 };
 const wrapStackTokens: IStackTokens = { childrenGap: 10 };
 const counterStackTokens: IStackTokens = {
@@ -71,10 +72,14 @@ const counterStackTokens: IStackTokens = {
 };
 const lockStackTokens: IStackTokens = {
   childrenGap: 5,
-  padding: 20
-}
+  padding: 20,
+};
 
 const styles: Partial<ISpinButtonStyles> = { spinButtonWrapper: { width: 75 } };
+
+interface IPLayConfig {
+  play: number;
+}
 
 interface ICounter {
   firstCounter?: string | undefined;
@@ -83,174 +88,240 @@ interface ICounter {
 }
 
 interface ICombination {
-  combinationIsSet: boolean,
-  cmb: string | undefined | null
+  combinationIsSet: boolean;
+  cmb: string | undefined | null;
+}
+
+interface IUnlocked {
+  unlocked: boolean;
+  unlk: number
 }
 
 const App: React.FC = () => {
-
-  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(false)
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(false);
   const [combination, setCombinationIsSet] = useState<ICombination>({
     combinationIsSet: false,
-    cmb: null
-  })
-
+    cmb: null,
+  });
   const [value, setValue] = useState<ICounter>({
     firstCounter: "0",
     secondCounter: "0",
     thirdCounter: "0",
   });
 
+  const [playConfig, setPlayConfig] = useState<IPLayConfig>({
+    play: 0,
+  });
 
-function onChange(ev: React.MouseEvent<HTMLElement>, checked?: boolean) {
+  const [unlockConfig, setUnlockConfig ] = useState<IUnlocked>({ unlocked: false, unlk: 0 })
+
+  function onChange(ev: React.MouseEvent<HTMLElement>, checked?: boolean) {
     if (checked) {
-      setIsDarkTheme(!isDarkTheme)
+      setIsDarkTheme(!isDarkTheme);
     } else {
-      setIsDarkTheme(!isDarkTheme)
+      setIsDarkTheme(!isDarkTheme);
     }
-}
-
-const setCombination = () => {
-  if (value.firstCounter && value.secondCounter && value.thirdCounter!== undefined) {
-    if (combination.combinationIsSet) {
-      throw new Error("Combination already set")
-    } else {
-      const currentCombination: string = (value.firstCounter + value.secondCounter + value.thirdCounter)
-      if (currentCombination === "000") {
-        alert("can't set 000 as combination")
-      } else {  
-        setCombinationIsSet({
-          combinationIsSet: !combination.combinationIsSet,
-          cmb: currentCombination
-        })
-        setValue({
-          firstCounter: "0",
-          secondCounter: "0",
-          thirdCounter: "0",
-        })
-      }   
-    }    
   }
-}
 
-
-const unlock = () => {
-  if (combination.cmb !== null && combination.cmb !== undefined) {
-      if (value.firstCounter && value.secondCounter && value.thirdCounter!== undefined) {
-        const currentCombination: string = (value.firstCounter + value.secondCounter + value.thirdCounter)
-          if (currentCombination ===  combination.cmb) {
-                alert("Unlocked")
-          } else {
-                alert("Wrong Combination! Try again.")
-          }
+  const setCombination = () => {
+    if (
+      value.firstCounter &&
+      value.secondCounter &&
+      value.thirdCounter !== undefined
+    ) {
+      if (combination.combinationIsSet) {
+        throw new Error("Combination already set");
+      } else {
+        const currentCombination: string =
+          value.firstCounter + value.secondCounter + value.thirdCounter;
+        if (currentCombination === "000") {
+          alert("can't set 000 as combination");
+        } else {
+          setCombinationIsSet({
+            combinationIsSet: !combination.combinationIsSet,
+            cmb: currentCombination,
+          });
+          setValue({
+            firstCounter: "0",
+            secondCounter: "0",
+            thirdCounter: "0",
+          });
+        }
       }
-  }
-}
+    }
+  };
+
+  const unlock = () => {
+    if (combination.cmb !== null && combination.cmb !== undefined) {
+      if (
+        value.firstCounter &&
+        value.secondCounter &&
+        value.thirdCounter !== undefined
+      ) {
+        const currentCombination: string =
+          value.firstCounter + value.secondCounter + value.thirdCounter;
+        if (currentCombination === combination.cmb) {
+          if (unlockConfig.unlocked) {
+              alert("App Already unlocked")
+          } else {
+            let newUnlk = unlockConfig.unlk + 1
+            setUnlockConfig({
+              unlocked: true,
+              unlk: newUnlk
+            })
+          }
+        
+        } else {
+          let newPlay = playConfig.play + 1;
+          setPlayConfig({ play: newPlay });
+        }
+      }
+    }
+  };
 
   return (
-    <ThemeProvider applyTo={"body"} theme={isDarkTheme? darkTheme: lightTheme}>
-    <div className="App">
-      <div className="App-body">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <Lock />
-        <Toggle className="toggle-btn" label="Dark mode" inlineLabel onText="On" offText="Off" onChange={onChange} />
-      </header>
-        <Stack tokens={containerStackTokens}>
-          <Stack
-            horizontal
-            horizontalAlign="center"
-            disableShrink
-            className="display-counter"
-            tokens={counterStackTokens}
-          >
-            <span>
-              {" "}
-              <h1>{value?.firstCounter}</h1>
-            </span>
-            <span>
-              <h1>{value?.secondCounter}</h1>
-            </span>
-            <span>
-              {" "}
-              <h1>{value?.thirdCounter}</h1>
-            </span>
-          </Stack>
+    <ThemeProvider
+      applyTo={"body"}
+      theme={isDarkTheme ? darkTheme : lightTheme}
+    >
+      <div className="App">
+        <div className="App-body">
+          <header className="App-header">
+            {unlockConfig.unlocked ? (
+              <Lock
+                key={unlockConfig.unlk}
+                play={playConfig.play}
+                unlocked={unlockConfig.unlocked}
+                unlk={unlockConfig.unlk}
+              />
+            ) : (
+              <Lock
+                key={playConfig.play}
+                play={playConfig.play}
+                unlocked={unlockConfig.unlocked}
+                unlk={unlockConfig.unlk}
+              />
+            )}
+            <Toggle
+              className="toggle-btn"
+              label="Dark mode"
+              inlineLabel
+              onText="On"
+              offText="Off"
+              onChange={onChange}
+            />
+          </header>
+          {unlockConfig.unlocked? (
+              <Stack
+              horizontal
+              horizontalAlign="center"
+              disableShrink
+              className="display-counter"
+              tokens={counterStackTokens}
+            >
+              <h1>:)</h1>
+          
+            </Stack>
+          ): (
+<Stack tokens={containerStackTokens}>
+            <Stack
+              horizontal
+              horizontalAlign="center"
+              disableShrink
+              className="display-counter"
+              tokens={counterStackTokens}
+            >
+              <span>
+                {" "}
+                <h1>{value?.firstCounter}</h1>
+              </span>
+              <span>
+                <h1>{value?.secondCounter}</h1>
+              </span>
+              <span>
+                {" "}
+                <h1>{value?.thirdCounter}</h1>
+              </span>
+            </Stack>
 
-          <Stack
-            horizontal
-            wrap
-            horizontalAlign="center"
-            tokens={wrapStackTokens}
-          >
-            <Stack.Item>
-              <SpinButton
-                value={value.firstCounter}
-                min={0}
-                max={9}
-                step={1}
-                incrementButtonAriaLabel="Increase value by 1"
-                decrementButtonAriaLabel="Decrease value by 1"
-                onChange={(e, newValue) => {
-                  if (newValue !== undefined) {
-                    setValue({ ...value, firstCounter: newValue });
-                  }
-                }}
-                styles={styles}
-              />
-            </Stack.Item>
-            <Stack.Item>
-              <SpinButton
-                value={value?.secondCounter}
-                min={0}
-                max={9}
-                step={1}
-                incrementButtonAriaLabel="Increase value by 1"
-                decrementButtonAriaLabel="Decrease value by 1"
-                onChange={(e, newValue) => {
-                  if (newValue !== undefined) {
-                    setValue({ ...value, secondCounter: newValue });
-                  }
-                }}
-                styles={styles}
-              />
-            </Stack.Item>
-            <Stack.Item>
-              <SpinButton
-                value={value?.thirdCounter}
-                min={0}
-                max={9}
-                step={1}
-                incrementButtonAriaLabel="Increase value by 1"
-                decrementButtonAriaLabel="Decrease value by 1"
-                onChange={(e, newValue) => {
-                  if (newValue !== undefined) {
-                    setValue({ ...value, thirdCounter: newValue });
-                  }
-                }}
-                styles={styles}
-              />
-            </Stack.Item>
-          </Stack>
-          <Stack
-            horizontal
-            horizontalAlign="center"
-            disableShrink
-            className="display-counter"
-            tokens={lockStackTokens}
-          >
-                 <PrimaryButton
-                  text="Set"
-                  allowDisabledFocus
-                  disabled={combination.combinationIsSet}
-                  onClick={setCombination}
+            <Stack
+              horizontal
+              wrap
+              horizontalAlign="center"
+              tokens={wrapStackTokens}
+            >
+              <Stack.Item>
+                <SpinButton
+                  value={value.firstCounter}
+                  min={0}
+                  max={9}
+                  step={1}
+                  incrementButtonAriaLabel="Increase value by 1"
+                  decrementButtonAriaLabel="Decrease value by 1"
+                  onChange={(e, newValue) => {
+                    if (newValue !== undefined) {
+                      setValue({ ...value, firstCounter: newValue });
+                    }
+                  }}
+                  styles={styles}
                 />
-                <DefaultButton text="Unlock" allowDisabledFocus onClick={unlock}/>
+              </Stack.Item>
+              <Stack.Item>
+                <SpinButton
+                  value={value?.secondCounter}
+                  min={0}
+                  max={9}
+                  step={1}
+                  incrementButtonAriaLabel="Increase value by 1"
+                  decrementButtonAriaLabel="Decrease value by 1"
+                  onChange={(e, newValue) => {
+                    if (newValue !== undefined) {
+                      setValue({ ...value, secondCounter: newValue });
+                    }
+                  }}
+                  styles={styles}
+                />
+              </Stack.Item>
+              <Stack.Item>
+                <SpinButton
+                  value={value?.thirdCounter}
+                  min={0}
+                  max={9}
+                  step={1}
+                  incrementButtonAriaLabel="Increase value by 1"
+                  decrementButtonAriaLabel="Decrease value by 1"
+                  onChange={(e, newValue) => {
+                    if (newValue !== undefined) {
+                      setValue({ ...value, thirdCounter: newValue });
+                    }
+                  }}
+                  styles={styles}
+                />
+              </Stack.Item>
+            </Stack>
+            <Stack
+              horizontal
+              horizontalAlign="center"
+              disableShrink
+              className="display-counter"
+              tokens={lockStackTokens}
+            >
+              <PrimaryButton
+                text="Set"
+                allowDisabledFocus
+                disabled={combination.combinationIsSet}
+                onClick={setCombination}
+              />
+              <DefaultButton
+                text="Unlock"
+                allowDisabledFocus
+                onClick={unlock}
+              />
+            </Stack>
           </Stack>
-
-        </Stack>
+          )}    
+        </div>
       </div>
-    </div>
     </ThemeProvider>
   );
 };
--- src/Lock.tsx
+++ src/Lock.tsx
@@ -1,62 +1,73 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const Lock: React.FC = () => {
+  interface IAnimation {
+      play: number;
+      unlocked: boolean;
+      unlk: number
+  }
+    
+
+const Lock: React.FC<IAnimation> = ({ play, unlocked, unlk }) => {
+
   return (
     <div className="svg-wrapper">
-      {/* <svg
-        width="100"
-        height="100"
-        viewBox="-50 -50 160 160"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <g id="lock">
-            
-        <motion.g animate={{
-            x: [-30, 0, 50, 35, 25, 15, 25],    
-        }}
-        transition={{
-            duration: 0.85,
-            ease: "linear",
-            repeatDelay: 2
-        }}
-        >
-          <rect id="lockBase" y="38" width="65" height="51" fill="#C4C4C4" />
-          <g id="lockHandle">
-            <path
-              id="Vector 1"
-              d="M11.5323 37.6452C11.5323 21.3511 15.7258 2 32.5 2C49.2742 2 53.4678 21.3511 53.4678 37.6452"
-              stroke="#C4C4C4"
-              strokeWidth="4"
-            />
-          </g>
-        </motion.g>
-        </g>
-      </svg> */}
       <svg
         width="83"
         height="120"
-        viewBox= "-41.5 -60 166 240"
+        viewBox="-41.5 -60 166 240"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <g id="padlock">
-          <path
-            id="handle"
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M42.0685 0C60.026 0 74.4828 14.57 74.4828 32.6682V68.9655H65.4789V32.6682C65.4789 19.5968 55.0374 9.07434 42.0685 9.07434C29.096 9.07434 18.6554 19.5968 18.6554 32.6682V57.6947H9.65517V32.6682C9.65517 14.57 24.1128 0 42.0685 0V0Z"
-            fill="#D1D5D5"
-          />
-          <rect
-            id="lockBase"
-            y="52.4138"
-            width="82.7586"
-            height="67.5862"
-            rx="3"
-            fill="#D1D5D5"
-          />
+          <motion.g
+            initial={(play > 0 && unlocked === false)? true : false}
+            animate={{
+              translateX: [null, -20, 0, 0],
+              fill: "#ff008c"
+            }}
+            transition={{
+              type: "spring",
+              repeat: 0,
+              repeatType: "mirror",
+              duration: 0.1,
+              stiffness: 80,
+            }}
+          >
+            <motion.g 
+            initial={(unlk > 0 )? true: false}
+            animate={{
+                y: [ -2.5, -15],
+                rotateY: 180
+            }}
+            transition={{
+                duration: 0.2,
+                delay: 0.1,
+                repeat: 0,
+                ease: "circIn"
+            }}
+            style={{
+                originY: "68px",
+                originX: "67px"
+            }}
+            >
+            <path
+              id="handle"
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M42.0685 0C60.026 0 74.4828 14.57 74.4828 32.6682V68.9655H65.4789V32.6682C65.4789 19.5968 55.0374 9.07434 42.0685 9.07434C29.096 9.07434 18.6554 19.5968 18.6554 32.6682V57.6947H9.65517V32.6682C9.65517 14.57 24.1128 0 42.0685 0V0Z"
+              fill="#D1D5D5"
+            />
+            </motion.g>
+            <rect
+              id="lockBase"
+              y="52.4138"
+              width="82.7586"
+              height="67.5862"
+              rx="3"
+              fill="#D1D5D5"
+            />
+          </motion.g>
         </g>
       </svg>
     </div>
